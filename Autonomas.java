cd8de2851cd678145f2109e42ed94abeb4534ee5
Kaylee Flanary <kflanary@glenrosetigers.info>
2018-04-16 10:25:21 -0500

    put code onto right side own switch

--- Autonomas.java
+++ Autonomas.java
@@ -68,15 +68,38 @@ public class Autonomas {
 		}
 	}
 	public void rightOwnLeftSide(){
-		
+		for (int i = 0; i < 4; i++)
+		{
+			// Owned switch right side, starting position left side
+			// Drive straight past switch, turn right, drive to right side of switch, turn right, drop cube
+			chassis.arcadeDrive(0.5, 0.0); // drive 50% fwd 0% turn
+			Timer.delay(2.0); // wait 2 seconds
+			chassis.arcadeDrive(0.0, 0.25); // drive 0% fwd, 25% turn
 	}
 	public void rightOwnCenter(){
+		// Owned switch right side, starting position middle side
+		// Robot needs to drive straight, turn right, drive straight past switch, turn left, drive straight past line, 
+		// turn left drive straight towards switch, turn left, drop cube in switch 
 		
+		chassis.arcadeDrive(0.5, 0.0); // drive 50% fwd 0% turn
+		Timer.delay(2.0); // wait 2 seconds
+		chassis.arcadeDrive(0.0, 0.15); // drive 0% fwd, 15% turn
+	}
+	chassis.arcadeDrive(0.0, 0.0); // drive 0% forward, 0% turn
 	}
 	public void rightOwnRightSide(){
-		
+		for (int i = 0; i < 4; i++)
+		{
+			// Owned switch right side, starting position right side
+			// Robot needs to drive straight, turn left towards switch, drop cube in switch, back up, turn right, drive out of way
+
+			chassis.arcadeDrive(0.5, 0.0); // drive 50% fwd 0% turn
+			Timer.delay(2.0); // wait 2 seconds
+			chassis.arcadeDrive(0.0, 0.75); // drive 0% fwd, 75% turn
+		}
+		chassis.arcadeDrive(0.0, 0.0); // drive 0% forward, 0% turn
 	}
-	public void noneSelected{
+	public void noneSelected(){
 		//ERROR Condition, a Start position was not Selected
 		//Attempt to drive straight and pass the switch only
 		if (timer.get() <= 1.5) {
