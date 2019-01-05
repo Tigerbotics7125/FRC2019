3192b5523c7af0a96094d04501299f8286208e9a
Kaylee Flanary <kflanary@glenrosetigers.info>
2019-01-05 14:34:33 -0600

    notes

--- Sensors.java
+++ Sensors.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
+
+// I dont know what sensors we have but this is where that stuff goes :)
 public class Sensors {
 	public Sensors() {
 	
